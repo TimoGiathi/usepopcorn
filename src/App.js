c50c71f28eb840f4d40d745ac585172c736d49fd
Timothy <lupingentleman123@gmail.com>
2025-01-27 20:24:00 +0300

    FEAT: Local storage for watched movies added

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ const KEY = "a51a1145";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [query, setQuery] = useState("");
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
 
+  const [watched, setWatched] = useState(function () {
+    const storedMovies = localStorage.getItem("wached");
+    return JSON.parse(storedMovies);
+  });
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
   }
@@ -27,6 +30,12 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+  useEffect(
+    function () {
+      localStorage.setItem("wached", JSON.stringify(watched));
+    },
+    [watched]
+  );
   useEffect(
     function () {
       const controller = new AbortController();
